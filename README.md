b968610dca3bb258e20fb600b2779114599c4f05
Eric <fingereric@web.de>
2021-07-23 20:40:10 +0200

    Re-wrote in safe rust, fixed tests & added some more test bf programs that are not yet handled (open while loops/mismatched brackets)

--- README.md
+++ README.md
@@ -5,17 +5,27 @@ A simple brainfuck interpreter written in Rust.
 
 - [X] Get the basic interpreter working
 - [X] Write Tests
-- [ ] Rewrite in safe Rust
+- [X] Rewrite in safe Rust
+- [X] Remove the panic by requiring a program in the constructor (thus preventing bad states)?
 - [ ] Write documentation
-- [ ] Remove the panic by requiring a program in the constructor (thus preventing bad states)?
-- [ ] Cache while-loop "jump points" (maybe even ahead of time?)
-- [ ] Run profiler to check performance, mb unsafe is faster?
-- [ ] Dynamically grow Memory
+- [ ] Cache while-loop "jump points" (maybe even ahead of time?) (Would allow us to catch "openEndedWhile.bf" and "headlessWhile.bf" / mismatched brackets in general)
+- [ ] Add better comments to brainfuck files (ignore stuff after ; when parsing)
+- [ ] Better Error-handling, backtraces for when bf programs crash etc.
+- [ ] Dynamically grow Memory/prevent overflow of Memory Pointer
 - [ ] Add support for different cell sizes
 - [ ] UI, loading Programms at runtime
-- [ ] Add better comments to brainfuck files (ignore stuff after ;)
-- [ ] Better Error-handling, backtraces for when bf programs crash etc.
+- [ ] Run profiler to check performance, mb unsafe is faster?
 - [ ] Better I/O for stuff like wc.bf or rot13.bf
 - [ ] Better I/O for tests
 - [ ] GUI?
 - [ ] Debugging capabilities (stepping, memory-state etc.)
+
+### Current State:
+
+- Has no UI
+- Can therefore only run hard-coded brainfuck
+- Does not handle mismatched While loops (Hangs or crashes)
+- Does ignore any non-opcode instructions but does not respect comments (lines starting with ; or // or # etc)
+- Has a default (hard-coded) & fixed memory-size of 1KB
+- A cell is a byte per brainfuck "spec" (8 bits)
+- Might not be the fastest (although with --release it's quite good already, IO is somewhat slow bc we print every char as per spec)
--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ use std::io::{BufRead, Write};
 struct VM {
     program: String,
     pp: usize, // ProgramPointer
+    mp: usize, // MemoryPointer
     data: Vec<u8>,
     #[cfg(test)]
     output: String,
 }
 
 impl VM {
-    pub fn new() -> Self {
+    pub fn new(program: &str) -> Self {
         // TODO: Dynamically grow this if needed & start with smaller defaults
         let data = vec![0; 1024];
         Self {
-            program: "".to_string(),
-            data,
+            program: program.to_string(),
             pp: 0,
+            mp: 0,
+            data,
             #[cfg(test)]
             output: String::new(),
         }
@@ -32,15 +34,7 @@ impl VM {
         self.output.clear();
     }
 
-    // TODO: rewrite in safe rust (without raw pointers!)
-    //       should be easy to do, add a data_pointer int and index the vec with it *shrugs*
-    pub unsafe fn run(&mut self) {
-        if self.program.is_empty() {
-            panic!("No Program specified!");
-        }
-
-        let mut pointer = self.data.as_mut_ptr();
-
+    pub fn run(&mut self) {
         while self.pp < self.program.len() {
             match self
                 .program
@@ -49,36 +43,36 @@ impl VM {
                 .expect("End of program reached prematurely!")
             {
                 '>' => { // pointer += 1;
-                    pointer = pointer.add(1);
+                    self.mp += 1;
                     self.pp += 1;
                 }
                 '<' => { // pointer -= 1;
-                    pointer = pointer.sub(1);
+                    self.mp -= 1;
                     self.pp += 1;
                 }
                 '+' => { // *pointer += 1;
-                    *pointer = (*pointer).wrapping_add(1);
+                    self.data[self.mp] = self.data[self.mp].wrapping_add(1);
                     self.pp += 1;
                 }
                 '-' => { // *pointer -= 1;
-                    *pointer = (*pointer).wrapping_sub(1);
+                    self.data[self.mp] = self.data[self.mp].wrapping_sub(1);
                     self.pp += 1;
                 }
                 '.' => { // putchar(*pointer)
-                    print!("{}", *pointer as char);
-                    std::io::stdout().lock().flush().expect("Couldn't lock stdout!");
+                    print!("{}", self.data[self.mp] as char);
+                    std::io::stdout().lock().flush().expect("Could not flush stdout");
                     #[cfg(test)]
-                    self.output.push(*pointer as char);
+                    self.output.push(self.data[self.mp] as char);
                     self.pp += 1;
                 }
                 ',' => { //getchar(*pointer)
                     let mut input = String::new();
-                    std::io::stdin().lock().read_line(&mut input).expect("Couldn't lock stdin");
-                    *pointer = input.chars().nth(0).expect("No input??") as u8;
+                    std::io::stdin().lock().read_line(&mut input).expect("Couldn't read from stdin");
+                    self.data[self.mp] = input.chars().nth(0).expect("No input could be read from stdin?") as u8;
                     self.pp += 1;
                 }
                 '[' => { // if *pointer == 0: goto end of while)
-                    if *pointer == 0 {
+                    if self.data[self.mp] == 0 {
                         let mut count: u32 = 0;
                         // TODO: Cache jump points once we found the matching ']' for quicker repeats (loops in loops)?
                         //       maybe even cache ALL jump points for all loops in the beginning of the program?? self.pp = address
@@ -109,7 +103,7 @@ impl VM {
                 ']' => { // } (or "if *pointer != 0: goto start of while")
                     // TODO: Rewrite so we add a "jump point" to a list when entering a while loop
                     // and don't have to scan the program for the beginning '[' every time
-                    if *pointer != 0 {
+                    if self.data[self.mp] != 0 {
                         let mut count: u32 = 0;
                         for x in 1 .. self.pp {
                             let current_char = self.program.chars().nth(self.pp - x).expect("didn't find matching '['!");
@@ -140,33 +134,31 @@ impl VM {
 }
 
 fn main() {
-    let mut vm = VM::new();
     let program = include_str!("../brainfuck_programs/hello_world.bf");
-    vm.load(program);
-    unsafe { vm.run() };
+    let mut vm = VM::new(program);
+    vm.run();
 
     vm.reset();
     let program = include_str!("../brainfuck_programs/yapi_4.bf");
     vm.load(program);
-    unsafe { vm.run() };
+    vm.run();
 
     vm.reset();
     let program = include_str!("../brainfuck_programs/triangle.bf");
     vm.load(program);
-    unsafe { vm.run() };
+    vm.run();
 }
 
 #[cfg(test)]
-
 mod tests {
     use super::*;
 
     #[test]
     fn reset() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/hello_world_smol.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         assert_eq!("hello world", vm.output);
     
@@ -185,18 +177,46 @@ mod tests {
     }
 
     #[test]
-    #[should_panic]
     fn no_program() {
-        let mut vm = VM::new();
-        unsafe { vm.run() };
+        let mut vm = VM::new("");
+        vm.run();
+
+        assert!(vm.output.is_empty() && vm.pp == 0 && vm.mp == 0);
+        
+        let mut zerod = true;
+        for b in vm.data {
+            if b != 0 {
+                zerod = false;
+            }
+        }
+        assert!(zerod);
+    }
+
+    #[test]
+    #[should_panic]
+    fn mem_pointer_underflow() {
+        let program = include_str!("../brainfuck_programs/underflowMP.bf");
+        let mut vm = VM::new(program);
+        vm.load(program);
+        vm.run();
+    }
+
+    #[test]
+    #[should_panic]
+    fn mem_pointer_overflow() {
+        // TODO: replace this (with an "out of memory check") when we implement dynamic memory sizes
+        let program = include_str!("../brainfuck_programs/overflowMP.bf");
+        let mut vm = VM::new(program);
+        vm.load(program);
+        vm.run();
     }
 
     #[test]
     fn hello_world() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/hello_world.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         // Super fucking weird, why tf is it \n\r??? It's from https://de.wikipedia.org/wiki/Brainfuck 
         assert_eq!("Hello World!\n\r", vm.output);
@@ -204,10 +224,10 @@ mod tests {
 
     #[test]
     fn hello_world_smol() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/hello_world_smol.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         assert_eq!("hello world", vm.output);
     }
@@ -215,20 +235,20 @@ mod tests {
     #[test]
     fn hell() {
         // "Hello world from hell": https://github.com/rdebath/Brainfuck/blob/master/bitwidth.b
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/hell.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         assert_eq!("Hello World! 255\n", vm.output);
     }
     
     #[test]
     fn squares() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/squares.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         let should_be = include_str!("../brainfuck_programs/squares_output_correct.txt");
         assert_eq!(should_be, vm.output);
@@ -237,30 +257,30 @@ mod tests {
     #[test]
     fn quine() {
         // Written by Erik Bosman
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/quine.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         assert_eq!(program, vm.output);
     }
 
     #[test]
     fn obscure() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/obscure.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         assert_eq!("H\n", vm.output);
     }
 
     #[test]
     fn fibonacci() {
-        let mut vm = VM::new();
         let program = include_str!("../brainfuck_programs/fibonacci.bf");
+        let mut vm = VM::new(program);
         vm.load(program);
-        unsafe { vm.run() };
+        vm.run();
 
         // yes those are wrong, but that's the programs fault. These numbers are from https://copy.sh/brainfuck which I assume is correct
         assert_eq!("1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 121, 98, 219, ...", vm.output);
