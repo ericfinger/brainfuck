851a24e0395447ff3b07352d6a8ce28741773ad6
Eric <fingereric@web.de>
2021-07-25 21:46:05 +0200

    Added comment parsing and improved performance
    - Everything after ';' and '#' in a line will now be ignored
    - Improved performance by indexing into a vector of bytes instead of
    iterating over the entire program-string, now that it's safe to assume
    that every character in the program after parsing is ascii. (Still want
    to check if a &str.to_bytes would be faster to index)

---
+++ brainfuck_programs/comments_ignored_poundsign.bf
@@ -0,0 +1,34 @@
++   # this should print and exclamation mark '!' to the console
++   # if it doesn't you didn't remove the comments correctly
++
++
++
++   
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++
++   
++   ## >
++   # > # >
++   # >
+.
--- src/brainfuck.rs
+++ src/brainfuck.rs
@@ -2,44 +2,37 @@ use std::io::{BufRead, Write};
 
 use bimap::BiMap;
 pub struct VM {
-    program: String,
-    pp: usize, // ProgramPointer
-    mp: usize, // MemoryPointer
+    program: Vec<u8>,
+    pp: usize,        // ProgramPointer
+    mp: usize,        // MemoryPointer
     data: Vec<u8>,
     jump_map: BiMap<usize, usize>,
+    // TODO: move this to some kind of config:
+    ignore_comments: bool, // wether we should ignore comments (obscure.bf and hell.bf use ';' as non-comment chars)
     #[cfg(test)]
     output: String,
 }
 
 impl VM {
     pub fn new(program: &str) -> Self {
-        // TODO: Dynamically grow this if needed & start with smaller defaults
-        let data = vec![0; 1024];
-        let mut jump_map = BiMap::<usize, usize>::new();
-
-        if !Self::parse(program, &mut jump_map) {
-            // TODO: Give better Debug output
-            panic!("Mismatched Brackets!");
-        }
-
-        Self {
-            program: program.to_string(),
+        let mut vm = Self {
+            program: Vec::<u8>::new(),
             pp: 0,
             mp: 0,
-            data,
-            jump_map,
+            data: vec![0; 1024], // TODO: Dynamically grow this (static analysis of program possible??) if needed & start with smaller defaults
+            jump_map: BiMap::<usize, usize>::new(),
+            ignore_comments: true,
             #[cfg(test)]
             output: String::new(),
-        }
+        };
+
+        vm.parse(program).expect("Couldn't parse program");
+        vm
     }
 
     pub fn load(&mut self, program: &str) {
-        if !Self::parse(program, &mut self.jump_map) {
-            // TODO: Give better ~~Debug~~ output
-            panic!("Mismatched Brackets!");
-        }
-
-        self.program = program.to_string();
+        self.program.clear();
+        self.parse(program).expect("Couldn't parse program");
     }
 
     pub fn reset(&mut self) {
@@ -50,48 +43,66 @@ impl VM {
         self.output.clear();
     }
 
+    /// Turns comment parsing off, meaning ';' and '#' no longer end the line.
+    /// Needs to reload the program since it's parsed on load.
+    /// TODO: Maybe save 'original' Program so we don't need to reload?
+    #[allow(dead_code)]
+    pub fn disable_commen_parsing(&mut self, program: &str) {
+        self.ignore_comments = false;
+        self.load(program);
+    }
+
+    /// Turns comment parsing back on, meaning ';' and '#' end the line.
+    /// Needs to reload the program since it's parsed on load.
+    /// TODO: Maybe save 'original' Program so we don't need to reload?
+    #[allow(dead_code)]
+    pub fn enable_commen_parsing(&mut self, program: &str) {
+        self.ignore_comments = true;
+        self.load(program);
+    }
+
     pub fn run(&mut self) {
         while self.pp < self.program.len() {
-            // TODO: after parsing (& therefore making sure our program is only ascii symbols) we should
-            //       index into program.as_bytes (or mb even turn the program into a vector?) for speed
-            match self
-                .program
-                .chars()
-                .nth(self.pp)
-                .expect("End of program reached prematurely!")
-            {
-                '>' => {
+            match self.program[self.pp] {
+                b'>' => {
                     // pointer += 1;
                     self.mp += 1;
                     self.pp += 1;
                 }
-                '<' => {
+
+                b'<' => {
                     // pointer -= 1;
                     self.mp -= 1;
                     self.pp += 1;
                 }
-                '+' => {
+
+                b'+' => {
                     // *pointer += 1;
                     self.data[self.mp] = self.data[self.mp].wrapping_add(1);
                     self.pp += 1;
                 }
-                '-' => {
+
+                b'-' => {
                     // *pointer -= 1;
                     self.data[self.mp] = self.data[self.mp].wrapping_sub(1);
                     self.pp += 1;
                 }
-                '.' => {
+
+                b'.' => {
                     // putchar(*pointer)
                     print!("{}", self.data[self.mp] as char);
                     std::io::stdout()
                         .lock()
                         .flush()
                         .expect("Could not flush stdout");
+
+                    self.pp += 1;
+
                     #[cfg(test)]
                     self.output.push(self.data[self.mp] as char);
-                    self.pp += 1;
                 }
-                ',' => {
+
+                b',' => {
                     //getchar(*pointer)
                     let mut input = String::new();
                     std::io::stdin()
@@ -105,7 +116,8 @@ impl VM {
                         as u8;
                     self.pp += 1;
                 }
-                '[' => {
+
+                b'[' => {
                     // if *pointer == 0: goto end of while)
                     if self.data[self.mp] == 0 {
                         self.pp = self
@@ -117,7 +129,8 @@ impl VM {
                         self.pp += 1;
                     }
                 }
-                ']' => {
+
+                b']' => {
                     // } (or "if *pointer != 0: goto start of while")
                     if self.data[self.mp] != 0 {
                         self.pp = self
@@ -136,29 +149,48 @@ impl VM {
         }
     }
 
-    // Parses the program and reports errors
-    // TODO: actually report errors & introduce Error Type
-    fn parse(program: &str, jump_map: &mut BiMap<usize, usize>) -> bool {
-        if !Self::check_brackets(program, jump_map) {
-            return false;
+    /// Parses the program and reports errors
+    /// TODO: actually report errors & introduce Error Type
+    fn parse(&mut self, program: &str) -> Result<(), Box<dyn std::error::Error>> {
+        for line in program.lines() {
+            for c in line.chars() {
+                match c {
+                    '<' | '>' | '+' | '-' | '[' | ']' | '.' | ',' => {
+                        self.program.push(c as u8);
+                    }
+
+                    ';' | '#' => {
+                        // ignore until EOL after comment char
+                        if self.ignore_comments {
+                            break;
+                        }
+                    }
+
+                    _ => continue,
+                }
+            }
         }
 
-        true
+        if !self.check_brackets() {
+            Err("Could not parse Program: Mismatched Brackets!")?
+        }
+
+        Ok(())
     }
 
     /// Checks if all '[' brackts have a matching ']' bracket.
     /// Inserts the index of each '[' and it's matching ']' bracket into the given BiMap
-    fn check_brackets(program: &str, jump_map: &mut BiMap<usize, usize>) -> bool {
+    fn check_brackets(&mut self) -> bool {
         let mut count = 0;
         let mut machting_bracket: Option<usize>;
-        for (i, op) in program.chars().enumerate() {
+        for (i, op) in self.program.iter().enumerate() {
             match op {
-                '[' => {
+                b'[' => {
                     machting_bracket = None;
-                    for (j, c) in program.chars().enumerate().skip(i + 1) {
-                        if c == '[' {
+                    for (j, c) in self.program.iter().enumerate().skip(i + 1) {
+                        if *c == b'[' as u8 {
                             count += 1;
-                        } else if c == ']' {
+                        } else if *c == b']' as u8 {
                             if count == 0 {
                                 machting_bracket = Some(j);
                                 break;
@@ -173,19 +205,25 @@ impl VM {
                             return false;
                         }
                         Some(j) => {
-                            //println!("('[' at {}): Found matching Bracket ']' at {}", i, j);
-                            jump_map.insert(i, j);
+                            self.jump_map.insert(i, j);
                         }
                     }
                 }
-                ']' => {
+
+                b']' => {
                     machting_bracket = None;
-                    for (j, c) in program.chars().rev().enumerate().skip(program.len() - i) {
-                        if c == ']' {
+                    for (j, c) in self
+                        .program
+                        .iter()
+                        .rev()
+                        .enumerate()
+                        .skip(self.program.len() - i)
+                    {
+                        if *c == ']' as u8 {
                             count += 1;
-                        } else if c == '[' {
+                        } else if *c == '[' as u8 {
                             if count == 0 {
-                                machting_bracket = Some(program.len() - j);
+                                machting_bracket = Some(self.program.len() - j);
                                 break;
                             } else {
                                 count -= 1;
@@ -198,13 +236,12 @@ impl VM {
                             return false;
                         }
                         Some(_j) => {
-                            //println!("('[' at {}): Found matching Bracket ']' at {}", i, j);
-
                             // Ignoring value for now bc we already added it in the open-Bracket loop
                             // but might be useful for debugger later
                         }
                     }
                 }
+
                 _ => continue,
             }
         }
@@ -220,13 +257,15 @@ mod tests {
     #[test]
     fn reset() {
         let program = include_str!("../brainfuck_programs/hello_world_smol.bf");
+        let program_vec: Vec<u8> = program.chars().map(|c| c as u8).collect();
         let mut vm = VM::new(program);
+
         vm.run();
 
         assert_eq!("hello world", vm.output);
 
         vm.reset();
-        assert_eq!(program, vm.program);
+        assert_eq!(program_vec, vm.program);
         assert_eq!("", vm.output);
         assert_eq!(0, vm.pp);
 
@@ -311,6 +350,43 @@ mod tests {
     }
 
     #[test]
+    fn comment_semicolon_ignored() {
+        let program = include_str!("../brainfuck_programs/comments_ignored_semicolon.bf");
+        let mut vm = VM::new(program);
+        vm.run();
+
+        assert_eq!("!", vm.output);
+    }
+
+    #[test]
+    fn comment_poundsign_ignored() {
+        let program = include_str!("../brainfuck_programs/comments_ignored_poundsign.bf");
+        let mut vm = VM::new(program);
+        vm.run();
+
+        assert_eq!("!", vm.output);
+    }
+
+    #[test]
+    fn comment_semipound_ignored() {
+        let program = include_str!("../brainfuck_programs/comments_ignored_semipound.bf");
+        let mut vm = VM::new(program);
+        vm.run();
+
+        assert_eq!("!", vm.output);
+    }
+
+    #[test]
+    fn comment_not_ignored() {
+        let program = include_str!("../brainfuck_programs/comments_ignored_semipound.bf");
+        let mut vm = VM::new(program);
+        vm.disable_commen_parsing(program);
+        vm.run();
+
+        assert_ne!("!", vm.output);
+    }
+
+    #[test]
     fn hello_world() {
         let program = include_str!("../brainfuck_programs/hello_world.bf");
         let mut vm = VM::new(program);
@@ -334,6 +410,7 @@ mod tests {
         // "Hello world from hell": https://github.com/rdebath/Brainfuck/blob/master/bitwidth.b
         let program = include_str!("../brainfuck_programs/hell.bf");
         let mut vm = VM::new(program);
+        vm.disable_commen_parsing(program);
         vm.run();
 
         assert_eq!("Hello World! 255\n", vm.output);
@@ -363,6 +440,7 @@ mod tests {
     fn obscure() {
         let program = include_str!("../brainfuck_programs/obscure.bf");
         let mut vm = VM::new(program);
+        vm.disable_commen_parsing(program);
         vm.run();
 
         assert_eq!("H\n", vm.output);
